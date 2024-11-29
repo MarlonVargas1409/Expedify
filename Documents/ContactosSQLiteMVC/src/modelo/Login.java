84f69464054c8f6ff3959558d5b9b5c9680a2824
Porfirio <Porfirioramirez@gmail.com>
2024-11-28 21:27:18 -0400

    primer commit

---
+++ Documents/ContactosSQLiteMVC/src/modelo/Login.java
@@ -0,0 +1,44 @@
+
+package modelo;
+
+
+public class Login {
+    int id;
+    String usuario;
+    String clave;
+
+    public Login() {
+    }
+
+    public Login(int id, String usuario, String clave) {
+        this.id = id;
+        this.usuario = usuario;
+        this.clave = clave;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    
+}
