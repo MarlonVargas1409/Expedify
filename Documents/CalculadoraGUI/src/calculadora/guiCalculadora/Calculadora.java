4b1c8ce73fde4f016e03c70cd3be4e4779cf245e
Porfirio-Ramirez <Porfirioramirez999@gmail.com>
2024-12-01 02:36:58 -0400

    Delete Documents/CalculadoraGUI directory

--- Documents/CalculadoraGUI/src/calculadora/guiCalculadora/Calculadora.java
+++
@@ -1,7 +0,0 @@
-package calculadora.guiCalculadora;
-
-public class Calculadora {
-   public double sumar(Double valor1, Double valor2){
-       return valor1 + valor2;
-   } 
-}
--- Documents/CalculadoraGUI/src/convertidorgrados/PruebaConvertirdorGrados.java
+++
@@ -1,9 +0,0 @@
-package convertidorgrados;
-
-public class PruebaConvertirdorGrados {
-    public static void main(String[] args) {
-        ConvertidorGrados cg = new ConvertidorGrados();
-        int farenheit = cg.convertidorCelciusAFarenheit(30);
-        System.out.println("Grados Farenheit  = " + farenheit );
-    }
-}
